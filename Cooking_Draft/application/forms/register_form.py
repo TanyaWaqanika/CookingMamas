b8fb065f14d2d09497e18fecefe683aee0760f8d
daliesandmentlegen <156833233+daliesandmentlegen@users.noreply.github.com>
2024-04-08 18:55:41 +0100

    Delete Cooking_Draft directory

--- Cooking_Draft/application/forms/register_form.py
+++
@@ -1,7 +0,0 @@
-from wtforms import StringField, SubmitField
-
-
-class RegisterForm(FlaskForm):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    submit = SubmitField('Add Name')
