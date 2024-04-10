538725f6e01f18b4b95b2db380b9555a5825af9d
Tanya Waqanika <twaqanika31@gmail.com>
2024-04-10 22:25:51 +0100

    commented code

--- application/dataAccess.py
+++ application/dataAccess.py
@@ -1,5 +1,9 @@
 import mysql.connector
 
+# connects website to database, database is called repdb bc I had a split second of dyslexia and forgot how recipe is
+# spelt smh
+
+
 recipedb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -9,6 +13,7 @@ recipedb = mysql.connector.connect(
 
 mycursor = recipedb.cursor()
 
+#  Shows tables available in db in python terminal
 
 mycursor.execute("SHOW TABLES")
 
